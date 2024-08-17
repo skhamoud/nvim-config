dcbc1ca2b5d6b8a0ef3aa80e329de9d47ee66917
Hamoud Khatar <skhamoud@protonmail.com>
2024-08-17 19:23:20 +0000

    update colorscheme to transparent bg

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  -- make tokyonight transparent bg
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
