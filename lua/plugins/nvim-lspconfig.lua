93863921e1f795998d70e046d0a8b4f901f1fda4
Hamoud Khatar <skhamoud@protonmail.com>
2025-01-25 14:09:47 +0000

    add ts-tools

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,8 +2,28 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      servers = { eslint = {} },
+      servers = {
+        -- eslint = {
+        --   enabled = true,
+        -- },
+        -- tsserver = {
+        --   enabled = false,
+        -- },
+        -- ts_ls = {
+        --   enabled = false,
+        -- },
+        -- vtsls = {
+        --   enabled = false,
+        -- },
+      },
       setup = {
+        tsserver = {
+          settings = {
+            seperate_diagnostic_server = true,
+            -- "change"|"insert_leave" determine when the client asks the server about diagnostic
+            publish_diagnostic_on = "insert_leave",
+          },
+        },
         eslint = function()
           require("lazyvim.util").lsp.on_attach(function(client)
             if client.name == "eslint" then
